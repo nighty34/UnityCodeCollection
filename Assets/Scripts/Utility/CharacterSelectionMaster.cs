44e18428ac73a47d297b4e40cc9ff02b472c481c
nightfury34 <47294479+nighty34@users.noreply.github.com>
2020-09-15 09:06:58 +0200

    Add CharacterSelection script

---
+++ Assets/Scripts/Utility/CharacterSelectionMaster.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelectionMaster : MonoBehaviour
+{
+    private CharacterSelection cs;
+    void Start(){
+        cs = GameObject.Find("CharacterSelection").GetComponent<CharacterSelection>();
+    }
+
+
+    public void IntoBattle(){
+        PlayerPrefs.SetInt("playerTankID", cs.Tankcounter);
+
+        //save Characters
+        
+        //Load Scene
+    }
+}
